804c01ec82e3e1a0ee902b66a78a466c85ad833f
ZirconiaCubed3v2 <lucian.adzima@gmail.com>
2024-02-23 11:04:14 -0800

    completed all the snippets to grab the info needed to build the database

--- scrape.py
+++ scrape.py
@@ -6,18 +6,27 @@ from bs4 import BeautifulSoup
 #     dex number (int)
 #     first dex entry listed (str)
 #     gen (int)
-#     type(s) (list of strings with one item if single-type and two items if dual-type)
+#     type(s) (list<str>; one item if single-type and two items if dual-type)
 #     species (str)
-#     height (int)
-#     weight (int)
-#     base stats (list; [HP, Attack, Defense, Sp. Atk, Sp. Def, Speed, Total])
-#     evolutions (dict; {(int)level: (str)name} for each evolution)
+#     height (float)
+#     weight (float)
+#     base stats (dict; keys: ["hp", "atk", "def", "spa", "spd", "spd", "tot"])
+#     evolutions (dict; {(int)order: [(str)name, (int)dexnum, (str)method]} for each evolution)
 
 # defining the final dict to contain all the data
-finalData = {"name": '', "dex": 0, "entry": '', "gen": 0, "types": [], "species": '', "height": 0, "weight": 0, "base_stats": [0, 0, 0, 0, 0, 0, 0], "evos": {}}
+finalData = {"name": '',
+             "dex": 0,
+             "entry": '',
+             "gen": 0,
+             "types": [],
+             "species": '',
+             "height": 0,
+             "weight": 0,
+             "base_stats": {"hp": 0, "atk": 0, "def": 0, "spa": 0, "sde": 0, "spd": 0, "tot": 0},
+             "evos": {}}
 
 # grab the url of the page
-URL = "https://pokemondb.net/pokedex/bulbasaur"
+URL = "https://pokemondb.net/pokedex/flabebe"
 page = requests.get(URL)
 
 # set up the parser
@@ -33,10 +42,56 @@ finalData["gen"] = int(''.join(i for i in soup.find("abbr").text if i.isdigit())
 finalData["entry"] = str(soup.findAll("td", class_="cell-med-text")[0].text)
 
 # now to get the types, which are <a> tags of class "type-icon type-{type}" with the text of the type, but thankfully they're in a fixed spot wrapped in a <td> tag
-types = []
 tempSoup = BeautifulSoup(''.join(str(child) for child in soup.findAll("td")[1]), "html.parser")
 for tag in tempSoup.findAll("a", class_="type-icon"):
-  types.append(tag.text.lower())
+  finalData["types"].append(tag.text.lower())
 
 # species isn't too hard either, it's just the third <td> tag on the page
 finalData["species"] = str(soup.findAll("td")[2].text).lower()
+
+# height and weight are going to be kept in metric measurements just to keep things simple
+finalData["height"] = float(soup.findAll("td")[3].text.split(" ", 1)[0][:-2])
+finalData["weight"] = float(soup.findAll("td")[4].text.split(" ", 1)[0][:-3])
+
+# base stats are in a table, not too hard to extract, just a lot of code repeating, but constructing a loop would take longer than just writing the code multiple times
+entries = BeautifulSoup(''.join(str(child) for child in soup.find("div", class_="grid-col span-md-12 span-lg-8")), "html.parser").findAll("tr")    # get all the table entries into a list
+for entry in entries:
+  tempSoup = BeautifulSoup(str(entry), "html.parser")
+  val = tempSoup.findAll("td", class_="cell-num")[0].text
+  match ((tempSoup.findAll("th")[0].text).lower()):
+    case "hp":
+      finalData["base_stats"]["hp"] = val
+    case "attack":
+      finalData["base_stats"]["atk"] = val
+    case "defense":
+      finalData["base_stats"]["def"] = val
+    case "sp. atk":
+      finalData["base_stats"]["spa"] = val
+    case "sp. def":
+      finalData["base_stats"]["sde"] = val
+    case "speed":
+      finalData["base_stats"]["spd"] = val
+    case "total":
+      finalData["base_stats"]["tot"] = val
+
+# evolutions are to be fairly hard, because they're kinda buried under other tags and stuff, and the levels are in seperate card divs
+evos = {}
+cards = soup.findAll("div", class_="infocard")
+arrows = soup.findAll("span", class_="infocard infocard-arrow")
+counter = 1
+for card in cards[1:]:
+  tempSoup = BeautifulSoup(''.join(str(child) for child in card), "html.parser")
+  evos[counter] = {"name": str(tempSoup.find("a", class_="ent-name").text.lower()),
+               "num": int(tempSoup.find("small").text.lower()[1:]),
+               "mtd": ''
+              }
+  mtd = BeautifulSoup(''.join(str(child) for child in arrows[cards.index(card)-1]), "html.parser").find("small").text.lower().split(" ", 1)[1][:-1]
+  try:
+    int(mtd)
+    evos[counter]["mtd"] = "lvl" + str(mtd)
+  except Exception:
+    evos[counter]["mtd"] = str(mtd).replace(" ", "")
+  counter += 1
+finalData["evos"] = evos
+
+print(finalData)
